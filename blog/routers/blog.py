39509f876527518c4601bd16dadd0a5bc70c2902
SR <rahmansaqib17@gmail.com>
2021-12-28 12:48:57 +0530

    added routes behind authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status,HTTPException
-from .. import schemas, database, models
+from .. import schemas, database, models, oauth2
 from typing import List
 from sqlalchemy.orm import Session
 from ..repository import blog
@@ -12,21 +12,21 @@ router = APIRouter(
 
 
 @router.get("/getall", response_model=List[schemas.ShowBlog])
-def all_blogs(db: Session = Depends(get_db)):
+def all_blogs(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 @router.get("/get/{id}", status_code=200, response_model=schemas.ShowBlog)
-def one_blog(id: int,db: Session = Depends(get_db)):
+def one_blog(id: int,db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get(id,db)
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
-def create_blog(new_blog: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(new_blog: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(new_blog, db)
 
 @router.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: int, db: Session = Depends(get_db)):
+def delete(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.delete(id,db)
 
 @router.put("/update/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog,db: Session = Depends(get_db)):
+def update(id: int, request: schemas.Blog,db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request,db)
